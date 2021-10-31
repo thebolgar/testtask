c36ab5e15a137cf5a28bf142ca9cfd569abb8f1f
thebolgar <lvl.up.00300@gmail.com>
2021-10-31 13:20:33 +0200

    layout

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+var swiper = new Swiper(".mySwiper", {  
+    loop: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+
+let date = new Date('Sep 3 2022 00:00:00');
+
+function counts() {
+    let now = new Date();
+    gap = date - now;
+
+    let days = Math.floor(gap / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(gap / 1000 / 60 / 60) % 24;
+    let minutes = Math.floor(gap / 1000 / 60) % 60;
+    let seconds = Math.floor(gap / 1000) % 60;
+
+    if (gap < 0) {
+        days = days + 7;
+        hours = hours + 24;
+        minutes = minutes + 60;
+        seconds = seconds + 60;
+        
+    } else {
+        document.getElementById('d').innerText = days;
+        document.getElementById('h').innerText = hours;
+        document.getElementById('m').innerText = minutes;
+        document.getElementById('s').innerText = seconds;
+    }
+}
+counts();
+
+setInterval(counts, 1000)
+
+
+
